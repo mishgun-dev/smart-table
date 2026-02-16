4476cbd27edbc0fbee639ceb0a3315d7edfae8a9
Mikhail Podkhalyuzin <tlllw@yandex.ru>
2026-02-16 20:34:49 +0300

    правки

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -28,6 +28,6 @@ export function initFiltering(elements, indexes) {
     }
 
     // @todo: #4.5 — отфильтровать данные используя компаратор
-    return data.filter((row) => compare(row, state));
+    return data.filter(row => compare(row, state));
   };
 }
